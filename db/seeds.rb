228e13ab2f3bf24ef5f86ab431053b294603c4c8
Kulas115 <kulas115@gmail.com>
2018-03-11 21:48:00 +0100

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Product.delete_all
+#
+Product.create!(title: "Seven Mobile Apps",
+  description:
+    %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text,More text.
+      </p>},
+    image_url: '',
+    price: 26.00)
